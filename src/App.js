7d5aa6a37de09fbc855bda040b2992c48e0e352a
moataz nasr <motaznasr44@gmail.com>
2021-08-21 01:12:54 +0200

    type:feat subject: add methods to bookshelf component body: added componentDidMount method to initialize booshelf state then added update method in bookshelf to make updates in parent and occurs at children Signed-off-by: moataz nasr <motaznasr44@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
-import {Route} from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import MainPage from './components/mainpage/MainPage'
 import SearchPage from './components/searchpage/SearchPage'
 
 class BooksApp extends React.Component {
 
   state = {
-  
+
     books: [],
 
   }
 
+  componentDidMount() {
+
+    BooksAPI.getAll()
+      .then((books) => {
+
+        this.setState(() => ({
+
+          books
+
+        }))
+
+      })
+
+  }
+
+  updateBookState = (book, shelf) => {
+
+    BooksAPI.update(book, shelf)
+      .then(() => {
+
+        const newBooks = this.state.books.filter(oldBook => oldBook.id !== book.id);
+
+        this.setState((prevState) => ({
+
+          books: [...newBooks, book]
+
+        }))
+
+      })
+
+
+  }
+
+
+
   render() {
     return (
-     
-      <div></div>
+
+      <div>
+
+        <MainPage books={this.state.books} updateBookState={this.updateBookState} />
+
+      </div>
     )
   }
 }
